c88f3c7e76ed685659a6a930754d9d8a6b2e6030
azibit <abdulazeezidris28@gmail.com>
2022-09-07 10:51:58 -0500

    Update dataset to use dataset from folder

--- utils.py
+++ utils.py
@@ -2,24 +2,47 @@ from PIL import Image
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
 
+from torch.utils.data import Dataset
+import glob as glob
+import os, cv2, random, torch
 
-class CIFAR10Pair(CIFAR10):
-    """CIFAR10 Dataset.
-    """
 
-    def __getitem__(self, index):
-        img, target = self.data[index], self.targets[index]
-        img = Image.fromarray(img)
+# class CIFAR10Pair(CIFAR10):
+#     """CIFAR10 Dataset.
+#     """
+#
+#     def __getitem__(self, index):
+#         img, target = self.data[index], self.targets[index]
+#         img = Image.fromarray(img)
+#
+#         if self.transform is not None:
+#             pos_1 = self.transform(img)
+#             pos_2 = self.transform(img)
+#
+#         if self.target_transform is not None:
+#             target = self.target_transform(target)
+#
+#         return pos_1, pos_2, target
+
+class EndoscopyDataset(Dataset):
+    def __init__(self, root_data, transform=None, no_of_views=2):
+        self.root_data = root_data
+        self.file_list = glob.glob(f"{root_data}/images/*")
+        self.transform = transform
+        self.no_of_views = no_of_views
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, idx):
+        img_loc = os.path.join(self.file_list[idx])
+        orig_image = cv2.cvtColor(cv2.imread(img_loc), cv2.COLOR_BGR2RGB)
 
         if self.transform is not None:
-            pos_1 = self.transform(img)
-            pos_2 = self.transform(img)
+            pos_1 = self.transform(transforms.ToPILImage()(orig_image))
+            pos_2 = self.transform(transforms.ToPILImage()(orig_image))
 
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return pos_1, pos_2, target
-
+        return pos_1, pos_2, idx
 
 train_transform = transforms.Compose([
     transforms.RandomResizedCrop(32),
